a2f2f347523e3cded4f891270a8c30ad588a014a
connorworthen <connorw220@protonmail.com>
2020-09-29 15:57:44 -0700

    icon not popping up

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM content has loaded");
-  let url = "http://localhost:3000/restaurants";
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((data) =>
-      data.forEach((restaurant) => {
-        displayRow(restaurant);
-      })
-    );
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("DOM content has loaded");
+//   let url = "http://localhost:3000/restaurants";
+//   fetch(url)
+//     .then((resp) => resp.json())
+//     .then((data) =>
+//       data.forEach((restaurant) => {
+//         displayRow(restaurant);
+//       })
+//     );
+// });
 
-function displayRow(row) {
-  const table = document.querySelector("#restaurant-table");
-  table.innerHTML += `
-  <tr>
-    <td>${row.name}</td><br>
-    <td>${row.address}</td>
-    <td>${row.closing_time}</td>
-    <td>${row.opening_time}</td>
-    <td>${row.price_range}</td>
-    <td>${row.category}</td>
-  </tr>
-  `;
-}
+// function displayRow(row) {
+//   const table = document.querySelector("#restaurant-table");
+//   table.innerHTML += `
+//   <tr>
+//     <td>${row.name}</td><br>
+//     <td>${row.address}</td>
+//     <td>${row.closing_time}</td>
+//     <td>${row.opening_time}</td>
+//     <td>${row.price_range}</td>
+//     <td>${row.category}</td>
+//   </tr>
+//   `;
+// }
 
 function myFunction() {
   // Declare variables
@@ -46,10 +46,36 @@ function myFunction() {
   }
 }
 
-document.getElementById(`button`).addEventListener("click", function () {
-  document.querySelector(`.bg-modal`).style.display = "flex";
-});
+// document.getElementById(`button`).addEventListener("click", function () {
+//   document.querySelector(`.bg-modal`).style.display = "flex";
+// });
 
-document.querySelector(`.close`).addEventListener("click", function () {
-  document.querySelector(`.bg-modal`).style.display = "none";
-});
+// document.querySelector(`.close`).addEventListener("click", function () {
+//   document.querySelector(`.bg-modal`).style.display = "none";
+// });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
