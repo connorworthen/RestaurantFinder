2c104ddd30bd04f45f8cb3a69b1faef11ad63965
connorworthen <connorw220@protonmail.com>
2020-09-29 13:09:41 -0700

    adding pop up front end

--- Backend/app/controllers/users_controller.rb
+++ Backend/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def index
+    user = User.all
+    render json: user
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+  
 end
