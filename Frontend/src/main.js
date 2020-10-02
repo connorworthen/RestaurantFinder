dfb2fed8c3742acc2e697407ba41dfe112502aa2
connorworthen <connorw220@protonmail.com>
2020-10-01 19:33:42 -0700

    sorting list

---
+++ Frontend/src/main.js
@@ -0,0 +1,5 @@
+const list = document.querySelector(".list");
+
+const sort_name_btn = document.querySelector(".sort-options .sort-name");
+const sort_name_btn = document.querySelector(".sort-options .sort-meta");
+const sort_name_btn = document.querySelector(".sort-options .sort-age");
