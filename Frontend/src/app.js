64cb9179c05b23f45ab5e3729d596d318a738301
connorworthen <connorw220@protonmail.com>
2020-10-20 13:27:44 -0700

    fixed error in naming

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,19 +1,25 @@
 const url = "http://localhost:3000/restaurants";
+const restaurantNameListDiv = document.getElementById("restaurant-names-list");
 
-function renderRestaurantName(RestaurantNameObject) {
-  const restaurantNameDiv = document.getElementById("restaurant-name-div");
-
-  const restaurantNameH3 = document.createElement("h3");
-  chatRoomNameH3.textContent = RestaurantNameObject.name;
-
-  restaurantNameDiv.prepend(restaurantNameH3);
+function renderRestaurantNamesList(restaurantNamesObject) {
+  restaurantNamesObject.forEach((restaurantNameObject) => {
+    renderRestaurantNameOnList(restaurantNameObject);
+  });
 }
 
-async function fetchRestaurants() {
-  const response = await fetch(url);
-  return await response.json();
+function renderRestaurantNameOnList(restaurantNameObject) {
+  const restaurantNameDiv = document.createElement("div");
+
+  restaurantNameDiv.innerHTML = `${restaurantNameObject.name}`;
+
+  restaurantNameListDiv.prepend(restaurantNameDiv);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetchRestaurants().then((results) => console.log(results));
-});
+// async function fetchRestaurants() {
+//   const response = await fetch(url);
+//   return await response.json();
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetchRestaurants().then((results) => console.log(results));
+// });
