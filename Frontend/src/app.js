dfe5c20fc6651c42996f484cdcae838d84c00215
connorworthen <connorw220@protonmail.com>
2020-10-20 17:45:28 -0700

    looping through result

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,5 +1,6 @@
 const url = "http://localhost:3000/restaurants";
-const restaurantNameListDiv = document.getElementById("restaurant-names-list");
+// const restaurantNameListDiv = document.getElementById("restaurant-names-list");
+const restaurantNameListDiv = document.querySelector(".products-center");
 
 function renderRestaurantNamesList(restaurantNamesObject) {
   restaurantNamesObject.forEach((restaurantNameObject) => {
@@ -10,9 +11,29 @@ function renderRestaurantNamesList(restaurantNamesObject) {
 function renderRestaurantNameOnList(restaurantNameObject) {
   const restaurantNameDiv = document.createElement("div");
 
-  restaurantNameDiv.innerHTML = `${restaurantNameObject.name}`;
+  restaurantNameDiv.innerHTML = `Restaurant: ${restaurantNameObject.name}`;
 
   restaurantNameListDiv.prepend(restaurantNameDiv);
+}
+
+function displayProduct(restaurantNamesObject) {
+  let result = "";
+  restaurantNamesObject.forEach((restaurantNameObject) => {
+    result += `
+        <article class="product">
+          <div class="img-container">
+            <img src=${restaurantNameObject.image} alt="product" class="product-img" />
+            <button class="bag-btn" data-id=${restaurantNameObject.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to cart
+            </button>
+          </div>
+          <h3>${restaurantNameObject.name}</h3>
+          <h3>${restaurantNameObject.address}</h3>
+        </article>
+      `;
+  });
+  restaurantNameListDiv.innerHTML = result;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
