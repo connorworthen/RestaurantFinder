f4115ac7d921b62485640a09f1833b33796bf170
connorworthen <connorw220@protonmail.com>
2020-10-22 16:18:41 -0700

    seeding db

--- Backend/app/controllers/restaurants_controller.rb
+++ Backend/app/controllers/restaurants_controller.rb
@@ -1,13 +1,11 @@
 class RestaurantsController < ApplicationController
 
   def index
-    @restaurants = Restaurant.all
-    render json: @restaurants
+    restaurants = Restaurant.all
+    render json: restaurants
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
-    render json: @restaurant
   end
 
   private
--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -1,9 +1,8 @@
-restaurant = Restaurant.new(
-  name:  "In n Out",
+test = Restaurant.create(
+  name:  "Test3",
   address: "456 Test Drive, San Diego, CA 12345",
   category: "Fast Food",
   opening_time: "8:00 A.M",
   closing_time: "2:00 A.M",
   price_range: 1
 )
-restaurant.image.attach(io: File.open('public/uploads/directory/product1.jpg'), filename: 'product1.jp')
