c353fa7f37c5ecf1b72cdd3824954c61405887af
connorworthen <connorw220@protonmail.com>
2020-10-22 14:26:10 -0700

    able to push images through seed data

--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -1,9 +1,9 @@
-Restaurant.create!(
+restaurant = Restaurant.new(
   name:  "In n Out",
   address: "456 Test Drive, San Diego, CA 12345",
   category: "Fast Food",
   opening_time: "8:00 A.M",
   closing_time: "2:00 A.M",
-  price_range: 1,
-  image: File.open(Rails.root.join('public/uploads/directory/product1.jpg')), filename: 'product1.jpg', content_type: 'image/jpg'
+  price_range: 1
 )
+restaurant.image.attach(io: File.open('public/uploads/directory/product1.jpg'), filename: 'product1.jp')
