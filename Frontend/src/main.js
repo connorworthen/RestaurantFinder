c0c586fe0437b0b1247f4032c95ea2f31a94a34c
connorworthen <connorw220@protonmail.com>
2020-10-23 17:36:29 -0700

    redoing home page

---
+++ Frontend/src/main.js
@@ -0,0 +1,27 @@
+// document.getElementById("button").addEventListener("click", function () {
+//   document.querySelector(".bg-modal").style.display = "flex";
+// });
+
+// window.onload = function () {
+//   document.querySelector(".close").addEventListener("click", function () {
+//     document.querySelector(".bg-modal").style.display = "none";
+//   });
+// };
+
+const toggleModal = () => {
+  document.querySelector(".modal").classList.toggle("modal--hidden");
+  document.querySelector(".overlay").classList.toggle("overlay--hidden");
+};
+document.querySelector("#show-modal").addEventListener("click", toggleModal);
+
+document.querySelector(".overlay").addEventListener("click", toggleModal);
+
+document.querySelector("#learn-more-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  toggleModal();
+});
+
+document
+  .querySelector(".modal__close-bar span")
+  .addEventListener("click", toggleModal);
+document.querySelector(".overlay").addEventListener("click", toggleModal);
--- Frontend/src/modal.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("button").addEventListener("click", function () {
-  document.querySelector(".bg-modal").style.display = "flex";
-});
-
-window.onload = function () {
-  document.querySelector(".close").addEventListener("click", function () {
-    document.querySelector(".bg-modal").style.display = "none";
-  });
-};
