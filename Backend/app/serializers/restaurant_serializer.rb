5b1df3b4737aa8a60598ed7fa6ba6d2afc04edff
connorworthen <connorw220@protonmail.com>
2020-10-23 18:55:08 -0700

    added image through active storage

--- Backend/app/serializers/restaurant_serializer.rb
+++ Backend/app/serializers/restaurant_serializer.rb
@@ -1,4 +1,12 @@
 class RestaurantSerializer
   # include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :address, :category, :closing_time, :opening_time, :price_range
+  attributes :id, :name, :address, :category, :closing_time, :opening_time, :price_range, :image
+
+  # include Rails.application.routes.url_helpers
+    
+  # attributes :image
+    
+  def image
+      rails_blob_path(object.image, only_path: true) if object.image.attached?
+  end
 end
--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -1,8 +1,10 @@
-test = Restaurant.create(
-  name:  "Test3",
+@restaurant = Restaurant.create(
+  name:  "Test",
   address: "456 Test Drive, San Diego, CA 12345",
   category: "Fast Food",
   opening_time: "8:00 A.M",
   closing_time: "2:00 A.M",
   price_range: 1
 )
+
+@restaurant.image.attach(io: File.open('./public/images/restaurant1.jpg'), filename: 'restaurant1.jpg', content_type: 'application/jpg')
