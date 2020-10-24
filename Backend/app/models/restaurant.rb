6fd3533ef2d22fe4ff5584eadc7bb56b0510bf0e
connorworthen <connorw220@protonmail.com>
2020-10-23 23:07:53 -0700

    trying img solution

--- Backend/app/models/restaurant.rb
+++ Backend/app/models/restaurant.rb
@@ -1,3 +1,12 @@
 class Restaurant < ApplicationRecord
   has_one_attached :image
+
+  # def image_attached(size_x, size_y)
+  #   if self.photos.length > 0
+  #     self.photos[0].variant(resize_to_limit: [size_x, size_y]).processed
+  #   else
+  #     "blank.jpg"
+  #   end
+  # end
+
 end
--- Backend/app/serializers/restaurant_serializer.rb
+++ Backend/app/serializers/restaurant_serializer.rb
@@ -1,11 +1,8 @@
 class RestaurantSerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
+
   attributes :id, :name, :address, :category, :closing_time, :opening_time, :price_range, :image
 
-  # include Rails.application.routes.url_helpers
-    
-  # attributes :image
-    
   def images
     rails_blob_path(object.image, only_path: true) if object.image.attached?
   end
