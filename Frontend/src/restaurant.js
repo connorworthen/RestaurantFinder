e01d50115369a67f3cd5f6e61af3dd18d212940a
connorworthen <connorw220@protonmail.com>
2020-11-03 17:47:09 -0800

    need to create function within onclick

--- Frontend/src/restaurant.js
+++ Frontend/src/restaurant.js
@@ -25,7 +25,6 @@ class Restaurant {
   }
 
   static addRestaurantModal(restaurant) {
-    // debugger;
     document.getElementById(`${restaurant.id}`).onclick = () => {
       document.getElementsByClassName("modal")[0].style.display = "block";
       document.getElementById(
@@ -57,27 +56,3 @@ class Restaurant {
       });
   }
 }
-
-// for (const restaurant of RestaurantNamesObject) { let restaurant_object = new Restaurant(restaurant['address'], restaurant['name']…} new_restaurant.renderRestaurant()restaurant_object.renderRestaurant()
-
-// static getRestaurant() {
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then(function (places) {
-//       let place_array = places["data"];
-//       Restaurant.renderRestaurant();
-
-//       for (let i = 0; i < place_array.length; i++) {
-//         let new_place = new Restaurant(
-//           place_array[i]["attributes"]["url"],
-//           place_array[i]["id"],
-//           place_array[i]["attributes"]["name"],
-//           place_array[i]["attributes"]["category"]
-//         );
-
-//         new_place.renderRestaurant();
-
-//         Restaurant.instances.push(new_place);
-//       }
-//     });
-// }
