b1f2103a6ce771e8c7fbe715f9a228f83a6f84ee
connorworthen <connorw220@protonmail.com>
2020-11-18 13:57:17 -0800

    description added to menu

--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_210150) do
+ActiveRecord::Schema.define(version: 2020_11_18_215057) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_10_22_210150) do
     t.string "drink"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "restaurants", force: :cascade do |t|
