3b0fb88317872a448f5beb00251529bb6aba54d8
connorworthen <connorw220@protonmail.com>
2020-11-18 13:36:20 -0800

    menu class + constructor

---
+++ Frontend/src/menu.js
@@ -0,0 +1,14 @@
+const url = "http://localhost:3000/menus";
+class Menu {
+  constructor(
+    appetizer,
+    entree,
+    dessert,
+    drink
+  ) {
+    this.appetizer = appetizer,
+    this.entree = entree,
+    this.dessert = dessert,
+    this.drink = drink
+  }
+}
