75d116435829c71098978e795d5803702daa501d
connorworthen <connorw220@protonmail.com>
2020-11-18 14:04:41 -0800

    menu article htmlg

--- Frontend/src/menu.js
+++ Frontend/src/menu.js
@@ -4,11 +4,53 @@ class Menu {
     appetizer,
     entree,
     dessert,
-    drink
+    drink,
+    description
   ) {
     this.appetizer = appetizer,
     this.entree = entree,
     this.dessert = dessert,
     this.drink = drink
+    this.description = description
+  }
+
+  renderMenu() {
+    document.getElementById("category").innerHTML += `
+      <article class="menu">
+        <div class="appetizer_menu">
+          <h3>Appetizers</h1>
+          <dl>
+            <dt>${this.appetizer[0]}</dt>
+              <dd>${this.description[0]}</dd>
+            <dt>${this.appetizer[1]}</dt>
+              <dd>${this.description[1]}</dd>
+          </dl>
+        </div>
+        <div class="entree_menu">
+          <h3>Entrees</h3>
+            <dt>${this.entree[0]}</dt>
+              <dd>${this.description[2]}</dd>
+            <dt>${this.entree[1]}</dt>
+              <dd>${this.description[3]}</dd>
+          </dl>
+        </div>
+        <div class="dessert_menu">
+          <h3>Desserts</h3>
+            <dt>${this.dessert[0]}</dt>
+              <dd>${this.description[4]}</dd>
+            <dt>${this.dessert[1]}</dt>
+              <dd>${this.description[5]}</dd>
+          </dl>
+        </div>
+        <div class="drink_menu">
+          <h3>Drinks</h3>
+            <dt>${this.drink[0]}</dt>
+              <dd>${this.description[6]}</dd>
+            <dt>${this.drink[1]}</dt>
+              <dd>${this.description[7]}</dd>
+          </dl>
+        </div>
+      </article>
+    `
   }
 }
