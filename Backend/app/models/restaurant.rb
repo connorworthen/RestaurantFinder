ff68ea174e23316e9cdc4a2731b0f6281a533084
connorworthen <connorw220@protonmail.com>
2020-11-18 13:26:09 -0800

    menu + restaurant relationship

--- Backend/app/models/restaurant.rb
+++ Backend/app/models/restaurant.rb
@@ -1,12 +1,5 @@
 class Restaurant < ApplicationRecord
   has_one_attached :image
-
-  # def image_attached(size_x, size_y)
-  #   if self.photos.length > 0
-  #     self.photos[0].variant(resize_to_limit: [size_x, size_y]).processed
-  #   else
-  #     "blank.jpg"
-  #   end
-  # end
+  has_one :menu
 
 end
