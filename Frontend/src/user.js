e9d1412a9424997c11cc26dfdb38e3b7e4da18da
connorworthen <connorw220@protonmail.com>
2020-11-19 16:57:11 -0800

    creating organized modal signup

--- Frontend/src/user.js
+++ Frontend/src/user.js
@@ -44,8 +44,6 @@ class User {
   static renderNewUser() {
     document.getElementsByClassName("modal-start")[0].innerHTML = `
       <form class="form-box">
-        <div class="close-x">X</div>
-        <h2 class="form-title">Welcome to Food Finder!</h2>
           <input type="text" placeholder="First Name *" class="form-input" value="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
 
           <input type="text" placeholder="Last Name *" class="form-input" value="">
