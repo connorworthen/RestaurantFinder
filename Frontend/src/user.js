77c397b0d721168e6099bd99a76f662f78bfd0bf
connorworthen <connorw220@protonmail.com>
2020-11-23 15:48:43 -0800

    backend recieves data

--- Frontend/src/user.js
+++ Frontend/src/user.js
@@ -46,9 +46,9 @@ class User {
 
           <input type="text" name="last_name" placeholder="Last Name *" class="form-input">
 
-          <input type="text" name="email" placeholder="Enter Email *" class="form-input">
+          <input type="email" name="email" placeholder="Enter Email *" class="form-input">
 
-          <input type="text" name="password" placeholder="Password *" class="form-input" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACIUlEQVQ4EX2TOYhTURSG87IMihDsjGghBhFBmHFDHLWwSqcikk4RRKJgk0KL7C8bMpWpZtIqNkEUl1ZCgs0wOo0SxiLMDApWlgOPrH7/5b2QkYwX7jvn/uc//zl3edZ4PPbNGvF4fC4ajR5VrNvt/mo0Gr1ZPOtfgWw2e9Lv9+chX7cs64CS4Oxg3o9GI7tUKv0Q5o1dAiTfCgQCLwnOkfQOu+oSLyJ2A783HA7vIPLGxX0TgVwud4HKn0nc7Pf7N6vV6oZHkkX8FPG3uMfgXC0Wi2vCg/poUKGGcagQI3k7k8mcp5slcGswGDwpl8tfwGJg3xB6Dvey8vz6oH4C3iXcFYjbwiDeo1KafafkC3NjK7iL5ESFGQEUF7Sg+ifZdDp9GnMF/KGmfBdT2HCwZ7TwtrBPC7rQaav6Iv48rqZwg+F+p8hOMBj0IbxfMdMBrW5pAVGV/ztINByENkU0t5BIJEKRSOQ3Aj+Z57iFs1R5NK3EQS6HQqF1zmQdzpFWq3W42WwOTAf1er1PF2USFlC+qxMvFAr3HcexWX+QX6lUvsKpkTyPSEXJkw6MQ4S38Ljdbi8rmM/nY+CvgNcQqdH6U/xrYK9t244jZv6ByUOSiDdIfgBZ12U6dHEHu9TpdIr8F0OP692CtzaW/a6y3y0Wx5kbFHvGuXzkgf0xhKnPzA4UTyaTB8Ph8AvcHi3fnsrZ7Wore02YViqVOrRXXPhfqP8j6MYlawoAAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
+          <input type="password" name="password" placeholder="Password *" class="form-input" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACIUlEQVQ4EX2TOYhTURSG87IMihDsjGghBhFBmHFDHLWwSqcikk4RRKJgk0KL7C8bMpWpZtIqNkEUl1ZCgs0wOo0SxiLMDApWlgOPrH7/5b2QkYwX7jvn/uc//zl3edZ4PPbNGvF4fC4ajR5VrNvt/mo0Gr1ZPOtfgWw2e9Lv9+chX7cs64CS4Oxg3o9GI7tUKv0Q5o1dAiTfCgQCLwnOkfQOu+oSLyJ2A783HA7vIPLGxX0TgVwud4HKn0nc7Pf7N6vV6oZHkkX8FPG3uMfgXC0Wi2vCg/poUKGGcagQI3k7k8mcp5slcGswGDwpl8tfwGJg3xB6Dvey8vz6oH4C3iXcFYjbwiDeo1KafafkC3NjK7iL5ESFGQEUF7Sg+ifZdDp9GnMF/KGmfBdT2HCwZ7TwtrBPC7rQaav6Iv48rqZwg+F+p8hOMBj0IbxfMdMBrW5pAVGV/ztINByENkU0t5BIJEKRSOQ3Aj+Z57iFs1R5NK3EQS6HQqF1zmQdzpFWq3W42WwOTAf1er1PF2USFlC+qxMvFAr3HcexWX+QX6lUvsKpkTyPSEXJkw6MQ4S38Ljdbi8rmM/nY+CvgNcQqdH6U/xrYK9t244jZv6ByUOSiDdIfgBZ12U6dHEHu9TpdIr8F0OP692CtzaW/a6y3y0Wx5kbFHvGuXzkgf0xhKnPzA4UTyaTB8Ph8AvcHi3fnsrZ7Wore02YViqVOrRXXPhfqP8j6MYlawoAAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
           
           <input type="submit" class="form-button" value="Create Account">
       </form>
@@ -61,17 +61,39 @@ class User {
   }
 
   static renderPostSignup() {
-    const formBox = document.getElementById('form-box');
-    formBox.onsubmit = function(e) {
-      e.preventDefault();
-      const request = new XMLHttpRequest();
-      request.open('POST', userUrl)
-      const formData = new FormData(document.getElementById("form-box"))
-      debugger
-      request.send(formData);
-      document.getElementsByClassName("signup-modal")[0].style.display = "none";
+    document.getElementById('form-box').onsubmit = function(e) {
+    let configObj = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            
+            body: JSON.stringify({
+                
+                user: {
+                    first_name: e.target['first_name'].value,
+                    last_name: e.target['last_name'].value,
+                    email: e.target['email'].value,
+                    password: e.target['password'].value
+                }    
+            })
+    };
+
+    fetch(userUrl, configObj) 
+      .then((response) => response.json())
+      .then((userData) => {
+        let newSignup = new Signup(
+            userData['data']['id'], 
+            userData['data']['attributes']['first_name'], 
+            userData['data']['attributes']['last_name'], 
+            userData['data']['attributes']['email'],
+            userData['data']['attributes']['password']
+        )
+        document.getElementsByClassName("signup-modal")[0].style.display = "none";
+        User.instances.push(newSignup)
+      })
     }
-  };
+  }
 
 
   static signinUser() {
