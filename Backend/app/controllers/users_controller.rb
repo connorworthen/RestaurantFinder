b9c3a3bdcbf0ffa47fcc5733189485d5500fa850
connorworthen <connorw220@protonmail.com>
2020-11-23 14:27:52 -0800

    routes for users

--- Backend/app/controllers/users_controller.rb
+++ Backend/app/controllers/users_controller.rb
@@ -1,19 +1,12 @@
 require 'pry'
 class UsersController < ApplicationController
 
-  def index
-    render json: User.all
-  end
-
   def create
     user = User.create(user_params)
     user.save
     render :json => user, status: :accepted
   end
-
-  def show
-  end
-
+  
   private
 
   def user_params
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # resources :restaurants, only: [:create, :index, :show]
   # resources :users, only: [:create]
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create' 
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :restaurants
   resources :users
