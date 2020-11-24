a32e1a6b295bee49e6ee71d71ed546b51fe11a32
connorworthen <connorw220@protonmail.com>
2020-11-23 16:15:21 -0800

    signin method

--- Frontend/src/user.js
+++ Frontend/src/user.js
@@ -1,4 +1,5 @@
-const userUrl = "http://localhost:3000/signup";
+const signupUrl = "http://localhost:3000/signup";
+const loginUrl = "http://localhost:3000/login";
 class User {
   constructor(
     first_name,
@@ -11,22 +12,6 @@ class User {
     this.email = email
     this.password = password
   }
-
-  // static fetchUsers() {
-  //   fetch(userUrl)
-  //     .then((response) => response.json())
-  //     .then((userData) => {
-  //         let newUser = new User(
-  //           userData["first_name"],
-  //           userData["last_name"],
-  //           userData["email"],
-  //           userData["password"]
-  //         );
-  //         // newUser.renderUser();
-  //         User.createUser();
-  //         User.instances.push(newUser);
-  //       })
-  //     }
 
   static createUser() {
     document.getElementById("form-button-signup").onclick = () => {
@@ -78,7 +63,7 @@ class User {
       })
     };
 
-    fetch(userUrl, configObj) 
+    fetch(signupUrl, configObj) 
       .then((response) => response.json())
       .then((userData) => {
         let newSignup = new Signup(
@@ -104,13 +89,13 @@ class User {
 
   static renderCurrentUser() {
     document.getElementsByClassName("signin-form")[0].innerHTML = `
-      <form class="form-box-signin">
+      <form class="form-box-signin" id="form-box-signin">
         <div class="close-form-signin">&times;</div>
         <h2 class="form-title-signin">Welcome back! Sign in below</h2>
 
-          <input type="text" placeholder="Enter Email *" class="form-input-signin" value="">
+          <input type="email" name="email" placeholder="Enter Email *" class="form-input">
 
-          <input type="text" placeholder="Password *" class="form-input-signin" value="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACIUlEQVQ4EX2TOYhTURSG87IMihDsjGghBhFBmHFDHLWwSqcikk4RRKJgk0KL7C8bMpWpZtIqNkEUl1ZCgs0wOo0SxiLMDApWlgOPrH7/5b2QkYwX7jvn/uc//zl3edZ4PPbNGvF4fC4ajR5VrNvt/mo0Gr1ZPOtfgWw2e9Lv9+chX7cs64CS4Oxg3o9GI7tUKv0Q5o1dAiTfCgQCLwnOkfQOu+oSLyJ2A783HA7vIPLGxX0TgVwud4HKn0nc7Pf7N6vV6oZHkkX8FPG3uMfgXC0Wi2vCg/poUKGGcagQI3k7k8mcp5slcGswGDwpl8tfwGJg3xB6Dvey8vz6oH4C3iXcFYjbwiDeo1KafafkC3NjK7iL5ESFGQEUF7Sg+ifZdDp9GnMF/KGmfBdT2HCwZ7TwtrBPC7rQaav6Iv48rqZwg+F+p8hOMBj0IbxfMdMBrW5pAVGV/ztINByENkU0t5BIJEKRSOQ3Aj+Z57iFs1R5NK3EQS6HQqF1zmQdzpFWq3W42WwOTAf1er1PF2USFlC+qxMvFAr3HcexWX+QX6lUvsKpkTyPSEXJkw6MQ4S38Ljdbi8rmM/nY+CvgNcQqdH6U/xrYK9t244jZv6ByUOSiDdIfgBZ12U6dHEHu9TpdIr8F0OP692CtzaW/a6y3y0Wx5kbFHvGuXzkgf0xhKnPzA4UTyaTB8Ph8AvcHi3fnsrZ7Wore02YViqVOrRXXPhfqP8j6MYlawoAAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
+          <input type="password" name="password" placeholder="Password *" class="form-input" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACIUlEQVQ4EX2TOYhTURSG87IMihDsjGghBhFBmHFDHLWwSqcikk4RRKJgk0KL7C8bMpWpZtIqNkEUl1ZCgs0wOo0SxiLMDApWlgOPrH7/5b2QkYwX7jvn/uc//zl3edZ4PPbNGvF4fC4ajR5VrNvt/mo0Gr1ZPOtfgWw2e9Lv9+chX7cs64CS4Oxg3o9GI7tUKv0Q5o1dAiTfCgQCLwnOkfQOu+oSLyJ2A783HA7vIPLGxX0TgVwud4HKn0nc7Pf7N6vV6oZHkkX8FPG3uMfgXC0Wi2vCg/poUKGGcagQI3k7k8mcp5slcGswGDwpl8tfwGJg3xB6Dvey8vz6oH4C3iXcFYjbwiDeo1KafafkC3NjK7iL5ESFGQEUF7Sg+ifZdDp9GnMF/KGmfBdT2HCwZ7TwtrBPC7rQaav6Iv48rqZwg+F+p8hOMBj0IbxfMdMBrW5pAVGV/ztINByENkU0t5BIJEKRSOQ3Aj+Z57iFs1R5NK3EQS6HQqF1zmQdzpFWq3W42WwOTAf1er1PF2USFlC+qxMvFAr3HcexWX+QX6lUvsKpkTyPSEXJkw6MQ4S38Ljdbi8rmM/nY+CvgNcQqdH6U/xrYK9t244jZv6ByUOSiDdIfgBZ12U6dHEHu9TpdIr8F0OP692CtzaW/a6y3y0Wx5kbFHvGuXzkgf0xhKnPzA4UTyaTB8Ph8AvcHi3fnsrZ7Wore02YViqVOrRXXPhfqP8j6MYlawoAAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
           
           <input type="submit" class="form-button-signin" value="Create Account">
       </form>
@@ -120,6 +105,35 @@ class User {
       document.getElementsByClassName("signin-modal")[0].style.display = "none";
     }
   }
+
+  static renderPostSignin() {
+    document.getElementById('form-box-signin').onsubmit = function(e) {
+    let configObj = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+            
+      body: JSON.stringify({    
+        user: {
+          email: e.target['email'].value,
+          password: e.target['password'].value
+        }    
+      })
+    };
+
+    fetch(signinUrl, configObj) 
+      .then((response) => response.json())
+      .then((userData) => {
+        let newSignin = new Signin(
+          userData['data']['attributes']['email'],
+          userData['data']['attributes']['password']
+        )
+        User.instances.push(newSignin)
+      })
+    }
+  }
+
 }
 
 function openNav() {
