09683c00f1c7e2d1af917ea6f9b231ee00e65176
connorworthen <connorw220@protonmail.com>
2020-11-29 12:52:24 -0800

    like has user_id

--- Backend/app/controllers/likes_controller.rb
+++ Backend/app/controllers/likes_controller.rb
@@ -1,4 +1,20 @@
 class LikesController < ApplicationController
 
-  
+  skip_before_action :authorized, only: [:index, :create]
+
+  def index
+    render json: Like.all
+  end
+
+  def create
+    like = Like.create(like_params)
+    like.save
+    render :json => like, status: :accepted
+  end
+
+  private
+
+  def like_params
+    params.permit(:user_id)
+  end
 end
