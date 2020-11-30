e17536c6d8145635ca995535f39f222aaf3a3d1a
connorworthen <connorw220@protonmail.com>
2020-11-29 21:07:53 -0800

    login accepts from js

--- Backend/app/controllers/users_controller.rb
+++ Backend/app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 require 'pry'
 class UsersController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create, :login]
 
   def create
     @user = User.create(user_params)
-    # binding.pry
     if @user.valid?
       @token = encode_token(user_id: @user.id)
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
@@ -13,20 +12,19 @@ class UsersController < ApplicationController
     end
   end
 
-  # def login
-  #   @user = User.find_by(email: params[:email])
-  #   if @user && @user.authenticate(params[:password])
-  #     token = encode_token({user_id: @user.id})
-  #     render json: {user: @user, token: token}
-  #   else
-  #     render json: {error: "Invalid username or password"}
-  #   end
-  # end
+  def login
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      token = encode_token({user_id: @user.id})
+      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+    else
+      render json: {error: "Invalid login username or password don't match.", status: :unauthorized}
+    end
+  end
 
-
-  # def auto_login
-  #   render json: user
-  # end
+  def auto_login
+    render json: @user
+  end
 
   private
 
