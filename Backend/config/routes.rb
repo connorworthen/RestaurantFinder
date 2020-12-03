6b15f236d39f066248796dca16f030072922c1dc
connorworthen <connorw220@protonmail.com>
2020-12-03 15:42:16 -0800

    favorites route

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  # get 'favorites/update'
   # resources :users, only: [:create, :login]
   post '/signup', to: 'users#create' 
   post '/login', to: 'users#login'
-  # get '/auto_login', to: 'users#login'
-  # delete '/logout', to: 'sessions#destroy'
-  
+  match "favorites/:restaurant_id" => "favorites#create", :as => :favorite
   resources :restaurants
   resources :users
-  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
