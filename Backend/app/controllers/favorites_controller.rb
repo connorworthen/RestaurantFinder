6cfea797a97cc851c8ca7d8ff9d31b9fbf43269f
connorworthen <connorw220@protonmail.com>
2020-12-03 17:04:19 -0800

    most recent push

--- Backend/app/controllers/favorites_controller.rb
+++ Backend/app/controllers/favorites_controller.rb
@@ -1,6 +1,4 @@
 class FavoritesController < ApplicationController
   def create
-    current_user.favorites.create(:restaurant_id => params[:restaurant_id])
-    render :layout => false
   end
 end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # resources :users, only: [:create, :login]
   post '/signup', to: 'users#create' 
   post '/login', to: 'users#login'
-  match "favorites/:restaurant_id" => "favorites#create", :as => :favorite
+
   resources :restaurants
   resources :users
+
+  resources :restaurants, only: [:index, :show] do
+    resources :users, only: [:favorite]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
