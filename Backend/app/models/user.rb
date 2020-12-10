6f5096e18cfdbee5559fee5dc4f591f5f6f5841e
connorworthen <connorw220@protonmail.com>
2020-12-10 10:19:11 -0800

    accepd nested attributes + reject method

--- Backend/app/models/user.rb
+++ Backend/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :favorites
+  accepts_nested_attributes_for :favorites, :reject_if => :reject_favorites
   has_many :restaurants, :through => :favorites
+
+  def reject_favorites(attributed)
+    attributed['favorited'].blank?
+  end
 end
