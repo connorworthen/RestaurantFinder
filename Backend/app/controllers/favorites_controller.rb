e8df5f6c56b5755b85960542d643cd206450ccb0
connorworthen <connorw220@protonmail.com>
2020-12-09 22:45:04 -0800

    bedtime push

--- Backend/app/controllers/favorites_controller.rb
+++ Backend/app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
-
   skip_before_action :authorized, only: [:update]
 
-  def update
-    
-    favorite.update_attributes(:favorited, [:true])
-    favrotie.save
-    render :json => favorite, status: :updated
+  def create
+    if Favorite.create(favorited: @restaurant, user: current_user)
+      redirect_to @restaurant, notice: 'Restaurant has been favorited'
+    else
+      redirect_to @project, alert: 'Something went wrong...'
+    end
   end
 end
