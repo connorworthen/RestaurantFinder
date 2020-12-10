43cc0d1866c92a926fa624fd9fb164bab770aea8
connorworthen <connorw220@protonmail.com>
2020-12-10 10:20:08 -0800

    fixed error

--- Backend/app/controllers/favorites_controller.rb
+++ Backend/app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   skip_before_action :authorized, only: [:update]
 
-  def create
+  def update
     if Favorite.create(favorited: @restaurant, user: current_user)
       redirect_to @restaurant, notice: 'Restaurant has been favorited'
     else
-      redirect_to @project, alert: 'Something went wrong...'
+      redirect_to @restaurant, alert: 'Something went wrong...'
     end
   end
 end
