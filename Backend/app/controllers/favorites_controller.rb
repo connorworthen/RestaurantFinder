82aabc61260b9d2075c4844c8fb7e16b97e0e6c6
connorworthen <connorw220@protonmail.com>
2020-12-09 22:00:57 -0800

    update

--- Backend/app/controllers/favorites_controller.rb
+++ Backend/app/controllers/favorites_controller.rb
@@ -3,6 +3,7 @@ class FavoritesController < ApplicationController
   skip_before_action :authorized, only: [:update]
 
   def update
+    
     favorite.update_attributes(:favorited, [:true])
     favrotie.save
     render :json => favorite, status: :updated
