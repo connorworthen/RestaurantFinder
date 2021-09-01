47e89c29cfc72a4ebd47f120cefb68639643f022
connorworthen <connorw220@protonmail.com>
2021-09-01 15:55:50 -0700

    output into html post filter

--- Frontend/src/search.js
+++ Frontend/src/search.js
@@ -3,19 +3,31 @@ class Search {
   static searchBar = document.getElementById("search-bttn")
 
   // Search restaurants.json and filter it
-  searchStates = async (searchText) => {
+  searchRestaurants = async (searchText) => {
     const res = await fetch('http://localhost:3000/restaurants')
     const data = await res.json()
-    
     // Get matches to current text input
     let matches = restaurants.filter(restaurant => {
       const regex = new Regex(`^${searchText}`, 'gi')
       return restaurant.name.match(regex) || restaurant.category.match(regex)
     })
-
-    
+    if (searchText.length === 0) {
+      matches = []
+    }
+    ouputHTML(matches)
   }
-  searchFunction() {
-    searchBar.addEventListener('input', () => searchRestaurants(searchBar.value))
+
+  // Show results in HTML
+  ouputHTML = (matches) => {
+    if (matches.length > 0) {
+      const HTML = matches.map(match => `
+        <div>
+          <h4>${match.name}</h4>
+        </div>
+      `)
+      .join('')
+
+      matchList.innerHTML = HTML
+    }
   }
 }
