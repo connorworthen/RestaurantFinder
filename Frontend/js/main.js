2378323f80d63ea3fed163436441689e78bef74c
connorworthen <connorw220@protonmail.com>
2021-09-03 17:08:42 -0700

    destructing function to save memory

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,20 +1,7 @@
 // const BASE_URL = "http://localhost:3000"
 // const restaurantService = new RestaurantService(BASE_URL)
 // restaurantService.fetchRestaurants()
-import * as User from './user.js'
-
-document.getElementById("signup-bttn").addEventListener("click", () => {
-  User.test()
+document.getElementById("signup-bttn").addEventListener('click', async () => {
+  const { renderForm } = await import('./user.js')
+  renderForm()
 })
-
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-  // User.createUser();
-  // User.signinUser();  
-  // User.logoutUser();
-  // User.userFavorites();
-  // Restaurant.instances = [];
-  // Restaurant.fetchRestaurants();
-  // checkAuth()
-// });
--- Frontend/js/user.js
+++ Frontend/js/user.js
@@ -1,30 +1,10 @@
 
-  let bttn = document.getElementById("signup-modal")
+function renderForm() {
+  document.getElementById("signup-bttn").innerHTML += `
+    <div>Test</div>
+  `
+}
 
-  function test() {
-    bttn.innerHTML = `
-      
-<div class="modal">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
 
-    `
-  }
 
-  export { test }
+export { renderForm }
